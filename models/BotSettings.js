10e09c660e5054b39ec15df0229d83c80b27c7fb
bielcalvoconstant-lab <bielcalvo.constant@gmail.com>
2026-06-24 17:08:36 -0300

    Update BotSettings.js

--- models/BotSettings.js
+++ models/BotSettings.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 
 const BotSettingsSchema = new mongoose.Schema({
   key: { type: String, default: 'global', unique: true },
-  activityName: { type: String, default: 'Bora trabalhar!' }, // Texto da atividade (Ex: "suporte")
-  activityType: { type: Number, default: 3 }, // 3 = Watching (Assistindo)
-  activityState: { type: String, default: 'Krypton | Atendimento Ativo' }, // Biografia / Custom Status personalizado
+  activityName: { type: String, default: 'Krypton Bot' }, // Título/Atividade
+  activityType: { type: Number, default: 4 }, // 4 = Custom Status (Status Personalizado estilo Loritta)
+  activityState: { type: String, default: 'Atendimento via Tickets ativo!' }, // Texto do Status
+  activityEmoji: { type: String, default: '💎' }, // Emoji do Status personalizado
   status: { type: String, default: 'online' } // online, idle, dnd, invisible
 });
 
